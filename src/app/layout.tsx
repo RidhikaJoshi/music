df70b80e654f92faf06739c694499986b88849a8
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-04-14 17:17:45 +0530

    Add Navbar component and related files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>This is a Navigation section.</nav>
+        <Navbar/>
         {children}
-        <footer>This is a footer section.</footer>
+       
         </body>
     </html>
   );
