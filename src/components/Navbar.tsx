57eea3423aba42d32cf717df209ab05c81d9d18d
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-04-15 06:18:35 +0530

    Update Navbar component and tailwind.config.ts file

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,30 +9,19 @@ function Navbar({ className }: { className?: string }) {
   return (
     <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
        <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Services">
+        <Link href={"/"}>
+        <MenuItem setActive={setActive} active={active} item="Home"></MenuItem></Link>
+        <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
+              <HoveredLink href="/courses">All Courses</HoveredLink>
+              <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+              <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+              <HoveredLink href="/courses">Songwriting</HoveredLink>
+              <HoveredLink href="/courses">Music Production</HoveredLink>
           </div>
         </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Products">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-          </div>
-        </MenuItem>
-        <MenuItem setActive={setActive} active={active} item="Pricing">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-          </div>
-        </MenuItem>
+        <Link href={"/contact"}>
+        <MenuItem setActive={setActive} active={active} item="Contact Us"></MenuItem></Link>
       </Menu>
     </div>
   )
